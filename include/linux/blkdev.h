70ffc77a08aeb07944fafdf2bad5670415fd44a6
Ming Lei <ming.lei@redhat.com>
2017-10-03 22:04:05 +0800

    block: support PREEMPT_ONLY
    When queue is in PREEMPT_ONLY mode, only REQ_PREEMPT request
    can be allocated and dispatched, other requests won't be allowed
    to enter I/O path.
    
    This is useful for supporting safe SCSI quiesce.
    
    Part of this patch is from Bart's '[PATCH v4 4∕7] block: Add the QUEUE_FLAG_PREEMPT_ONLY
    request queue flag'.
    
    Change-Id: Iffe29f0d6385e56bd6352c3f5c09a11346c12142
    Tested-by: Oleksandr Natalenko <oleksandr@natalenko.name>
    Tested-by: Martin Steigerwald <martin@lichtvoll.de>
    Cc: Bart Van Assche <Bart.VanAssche@wdc.com>
    Signed-off-by: Ming Lei <ming.lei@redhat.com>
    Patch-mainline: linux-block@vger.kernel.org @ 03/10/2017, 22:04
    Signed-off-by: Pradeep P V K <ppvk@codeaurora.org>
    Signed-off-by: engstk <eng.stk@sapo.pt>

--- include/linux/blkdev.h
+++ include/linux/blkdev.h
@@ -659,6 +659,7 @@ struct request_queue {
 #define QUEUE_FLAG_SCSI_PASSTHROUGH 27	/* queue supports SCSI commands */
 #define QUEUE_FLAG_QUIESCED    28	/* queue has been quiesced */
 #define QUEUE_FLAG_INLINECRYPT 29	/* inline encryption support */
+#define QUEUE_FLAG_PREEMPT_ONLY 30	/* only process REQ_PREEMPT requests */
 
 #define QUEUE_FLAG_DEFAULT	((1 << QUEUE_FLAG_IO_STAT) |		\
 				 (1 << QUEUE_FLAG_STACKABLE)	|	\
@@ -783,6 +784,10 @@ static inline void queue_throtl_add_request(struct request_queue *q,
 	((rq)->cmd_flags & (REQ_FAILFAST_DEV|REQ_FAILFAST_TRANSPORT| \
 			     REQ_FAILFAST_DRIVER))
 #define blk_queue_quiesced(q)	test_bit(QUEUE_FLAG_QUIESCED, &(q)->queue_flags)
+#define blk_queue_preempt_only(q)				\
+	test_bit(QUEUE_FLAG_PREEMPT_ONLY, &(q)->queue_flags)
+
+extern void blk_set_preempt_only(struct request_queue *q, bool preempt_only);
 
 static inline bool blk_account_rq(struct request *rq)
 {
