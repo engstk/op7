60eb069febd48cf76f2135c23cef91ca92dd33b1
engstk <eng.stk@sapo.pt>
2019-07-03 15:58:51 +0100

    input: touchscreen: add haptic feedback control sysfs
    echo 'Y|N' > /sys/module/touchpanel_common_driver/parameters/haptic_feedback_disable
    
    Signed-off-by: engstk <eng.stk@sapo.pt>

--- drivers/leds/aw8697.c
+++ drivers/leds/aw8697.c
@@ -64,6 +64,15 @@ struct pm_qos_request pm_qos_req_vb;
 /* add haptic audio tp mask */
 extern struct shake_point record_point[10];
 /* add haptic audio tp mask end */
+
+#ifdef CONFIG_HAPTIC_FEEDBACK_DISABLE
+int ignore_next_request = 0;
+void hap_ignore_next_request(void)
+{
+    ignore_next_request = 1;
+}
+#endif
+
 /******************************************************
  *
  * variable
@@ -3105,6 +3114,13 @@ static ssize_t aw8697_activate_store(struct device *dev,
 
     if (val != 0 && val != 1)
         return count;
+
+#ifdef CONFIG_HAPTIC_FEEDBACK_DISABLE
+    if ((ignore_next_request) && (val != 0)) {
+       ignore_next_request = 0;
+       return count;
+    }
+#endif
 
     pr_info("%s: value=%d\n", __FUNCTION__, val);
     mutex_lock(&aw8697->lock);
