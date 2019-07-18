8ac2d8ebea0da232adc372d1f7791c6ffe3371a6
LuK1337 <priv.luk@gmail.com>
2019-07-18 21:21:18 +0200

    drm: msm: Make sure to set proper dim_status if fp mode on as well
    Change-Id: I288883e744b8bd9787649358d7b33999e36c953d
    Signed-off-by: engstk <eng.stk@sapo.pt>

--- drivers/gpu/drm/msm/sde/sde_crtc.c
+++ drivers/gpu/drm/msm/sde/sde_crtc.c
@@ -5576,23 +5576,17 @@ static int sde_crtc_onscreenfinger_atomic_check(struct sde_crtc_state *cstate,
         if (mode ==3)
             aod_index = i;
 	}
-	if(fp_index >=0 && dim_mode!=0)
-		display->panel->dim_status = true;
-	else
-		display->panel->dim_status = false;
 
-		//if (fp_mode == 1 && sde_crtc_config_fingerprint_dim_layer(&cstate->base, cnt - 1)) {
-		//	pr_err("Failed to config dim layer\n");
-		//	return -EINVAL;
-		//}
-		if (fp_mode == 1) {
-			cstate->fingerprint_pressed = true;
-			return 0;
-		} else {
-			cstate->fingerprint_pressed = false;
-			cstate->fingerprint_dim_layer = NULL;
-			return 0;
-		}
+	if (fp_mode == 1) {
+		display->panel->dim_status = true;
+		cstate->fingerprint_pressed = true;
+		return 0;
+	} else {
+		display->panel->dim_status = false;
+		cstate->fingerprint_pressed = false;
+		cstate->fingerprint_dim_layer = NULL;
+		return 0;
+	}
 
 	if(aod_index <0){
 		oneplus_aod_hid = 0;
