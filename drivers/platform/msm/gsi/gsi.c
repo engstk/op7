0ca179fb7b7b6bdf45dbb33fd2894bfccdd21946
Adam W. Willis <return.of.octobot@gmail.com>
2020-04-28 22:09:31 -0400

    msm: gsi: Silence remaining loggers
    Signed-off-by: Adam W. Willis <return.of.octobot@gmail.com>
    Signed-off-by: engstk <eng.stk@sapo.pt>

--- drivers/platform/msm/gsi/gsi.c
+++ drivers/platform/msm/gsi/gsi.c
@@ -3020,7 +3020,7 @@ int gsi_stop_channel(unsigned long chan_hdl)
 	}
 
 	if (ctx->state == GSI_CHAN_STATE_STOP_IN_PROC) {
-		GSIERR("chan=%lu busy try again\n", chan_hdl);
+		GSIDBG("chan=%lu busy try again\n", chan_hdl);
 		res = -GSI_STATUS_AGAIN;
 		goto free_lock;
 	}
@@ -3089,7 +3089,7 @@ int gsi_stop_db_channel(unsigned long chan_hdl)
 	}
 
 	if (ctx->state == GSI_CHAN_STATE_STOP_IN_PROC) {
-		GSIERR("chan=%lu busy try again\n", chan_hdl);
+		GSIDBG("chan=%lu busy try again\n", chan_hdl);
 		res = -GSI_STATUS_AGAIN;
 		goto free_lock;
 	}
